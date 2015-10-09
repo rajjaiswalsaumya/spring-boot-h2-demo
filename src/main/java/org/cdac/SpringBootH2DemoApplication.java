25ea00db6e7a94da9ed146590efb6852d9f6518c
rajjaiswalsaumya <rajjaiswalsaumya@gmail.com>
2015-10-10 00:58:51 +0530

    Initial commit

---
+++ src/main/java/org/cdac/SpringBootH2DemoApplication.java
@@ -0,0 +1,11 @@
+package org.cdac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootH2DemoApplication.class, args);
+    }
+}
---
+++ src/test/java/org/cdac/SpringBootH2DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package org.cdac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootH2DemoApplication.class)
+@WebAppConfiguration
+public class SpringBootH2DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+
+
+}
